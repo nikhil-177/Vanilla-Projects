ccb2d8a10637266877b0b894643522508ee6e1f0
nikhil-177 <pn2683249@gmail.com>
2025-02-09 12:20:49 +0530

    Day-1 ends and project completed DO read README.md

--- Beginner/06_Weather_App/README.md
+++ Beginner/06_Weather_App/README.md
@@ -1,2 +1,13 @@
 # Day-1 :
 Hello there, Today we are going to make a weather app. As it is very good beginner app to make for increasing the knowledge of api's. so let's start it
+
+# FEATURES :
+- can search any city and will get its current weather status
+- it's resonsive also
+- it shows Humidity and wind speed too
+
+# Day-1 end :
+Today,we have completed this project it was a good project got to ler=arn many thing about it and got many errors too. But ok this is a part of life.
+
+# Project completed :
+Here by now, the project is completed and i have used chatgpt in some places too because it was my first time dealing with api's but I'll make sure i doesn't need it.THANKYOU!
--- Beginner/06_Weather_App/script.js
+++ Beginner/06_Weather_App/script.js
@@ -1,5 +1,49 @@
 
-
-const api = URL('https://api.openweathermap.org/data/2.5/weather?&units=metric')
+const api =  'https://api.openweathermap.org/data/2.5/weather?units=metric&q=';
 const apiKey = '719e5fa3c266003c0835e5f353a25d05';
 
+const searchedCity = document.getElementById('search');
+const btn = document.getElementById('btn');
+const img = document.getElementById('img');
+
+async function checkWeather(city) {
+    const response = await fetch(api + city + `&appid=${apiKey}`);
+    let data = await response.json()
+
+    if(response.status == 404){
+        document.querySelector(".error").style.display = 'block'
+    document.querySelector('.weather-city').style.display = 'none'
+    document.querySelector('.extra-info').style.display = 'none'
+    }
+    else{
+    document.querySelector('#name').innerHTML = data.name;
+    document.querySelector('#temp').innerHTML = Math.round(data.main.temp) + "°C";
+    document.querySelector('#humidities').innerHTML = data.main.humidity + "%";
+    document.querySelector('#wind').innerHTML = data.wind.speed + "km/h";
+    
+    if(data.weather[0].main == 'Clouds'){
+        img.src = "src/cloudy.png"
+    }
+    else if(data.weather[0].main == 'Clear'){
+        img.src = "src/clear.png"
+    }
+    else if(data.weather[0].main == 'Rain'){
+        img.src = "src/rain.png"
+    }
+    else if(data.weather[0].main == 'Drizzle'){
+        img.src = "src/drizzle.png"
+    }
+    else if(data.weather[0].main == 'Snow'){
+        img.src = "src/snow.png"
+    }
+
+    document.querySelector('.weather-city').style.display = 'block'
+    document.querySelector('.extra-info').style.display = 'flex'
+ }
+}
+
+
+btn.addEventListener('click', () => {
+    checkWeather(searchedCity.value);
+    
+})
