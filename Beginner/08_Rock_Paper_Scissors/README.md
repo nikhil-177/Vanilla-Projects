590fdb73e37ba6ddc27d2336e066024937fc89ea
nikhil-177 <pn2683249@gmail.com>
2025-02-12 12:15:35 +0530

    project completed

--- Beginner/08_Rock_Paper_Scissors/README.md
+++ Beginner/08_Rock_Paper_Scissors/README.md
@@ -1,2 +1,8 @@
 # Day-1:
 Hello there, Today we are going to make small rock,paper , scissors game . so let's start it,
+
+# day-1 end:
+So the day has ended as well as the project
+
+# Project completed:
+This project here has been completed. It wasnot a big project but was good to make. I'll make more awesome projects stay tuned. THANKYOU!
---
+++ Beginner/08_Rock_Paper_Scissors/script.js
@@ -0,0 +1,55 @@
+
+const playerDisplay = document.querySelector('#player-display')
+const computerDisplay = document.querySelector('#computer-display')
+const resultDisplay = document.querySelector('#result-display')
+const playerScore = document.querySelector('#player-score')
+const computerScore = document.querySelector('#computer-score')
+let ps = 0;
+let cs = 0;
+
+
+let choice = ["rock","paper","scissors"];
+
+function playerChoice(playerchoice){
+   
+    const computerChoice = choice[Math.floor(Math.random() * 3)]    
+    let result = "";
+    if(playerchoice === computerChoice){
+        result = "IT'S A TIE!"
+    }
+    else{
+        switch(playerchoice){
+            case "rock" :
+                result = (computerChoice == "paper") ? "YOU WIN!" : "YOU LOSE!"
+                break;
+            case "paper" :
+                result = (computerChoice == "scissors") ? "YOU WIN!" : "YOU LOSE!"
+                break;
+            case "scissors" :
+                result = (computerChoice == "rock") ? "YOU WIN!" : "YOU LOSE!"
+                break;
+        }
+    }
+
+    resultDisplay.textContent = result;
+    playerDisplay.textContent = `Player :${playerchoice}`;
+    computerDisplay.textContent = `Computer :${computerChoice}`;
+    resultDisplay.classList.remove("greentext","redtext")
+
+    switch(result){
+        case "YOU WIN!" :
+            resultDisplay.style.color = 'hsla(127, 100%, 25%, 0.818)';
+            ps++;
+            playerScore.textContent = `Player: ${ps}`;
+            break;
+        case "YOU LOSE!" :
+            resultDisplay.style.color = 'tomato';
+            cs++;
+            computerScore.textContent = `Computer: ${cs}`;
+            break;
+        case "IT'S A TIE!" :
+            resultDisplay.style.color = 'black';
+            break;
+    }
+
+}
