7a3611db01396ea28840c6990a692cb49dc5914f
nikhil-177 <pn2683249@gmail.com>
2025-02-15 08:59:01 +0530

    day- 1 ends

---
+++ Beginner/11_Responsive_Navbar/script.js
@@ -0,0 +1,7 @@
+
+const dropdown2 = document.querySelectorAll('#dropdown-2');
+const services = document.querySelector(' .services');
+
+services.addEventListener('click',() => {
+    dropdown2.style.display = 'block'
+})
