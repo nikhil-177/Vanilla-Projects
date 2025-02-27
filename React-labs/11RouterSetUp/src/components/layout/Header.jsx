4c0916e6db12d93496d22f0ac8e88f2ebd737c57
nikhil-177 <pn2683249@gmail.com>
2025-02-27 13:51:02 +0530

    added navbar to test active links

--- React-labs/11RouterSetUp/src/components/layout/Header.jsx
+++ React-labs/11RouterSetUp/src/components/layout/Header.jsx
@@ -1,6 +1,23 @@
+import { NavLink } from 'react-router-dom';
+import './Header.css';
 
 export const Header = () => {
     return (
-        <h1>Header </h1>
+        <div className='navbar'>
+            <h1>Logo</h1>
+            <nav>
+                <ul>
+                    <li>
+                        <NavLink to="/">Home</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="about">About</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="contact">Contact</NavLink>
+                    </li>
+                </ul>
+            </nav>
+        </div>
     )
 }
